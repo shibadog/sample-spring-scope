128c5b7152cb6a14829014d8944188f7dc1f4d79
shibadog <shiba_dog@outlook.com>
2021-02-12 20:09:02 +0900

    確認した

---
+++ README.md
@@ -0,0 +1,30 @@
+# Springのスコープの挙動を確かめたかった
+
+## overview
+
+Spring Bootで@Scopeを使って設定したインスタンが、  
+広いスコープのインスタンス内に狭いスコープのインスタンスに設定された場合の挙動について確認したかった。
+
+## 結果
+
+ちゃんとそれぞれのスコープを守っていい感じに処理される。
+
+```
+Controller: f98afab7-9557-43ec-9755-429734efff1d
+PCTX: fce145d7-8ddb-4290-9938-a30f0e7e7a69
+RCTX: 7068f4c3-2612-44e6-9fc1-5266c37dbe92
+SCTX: 92d5c2f0-df3c-4fbb-95cf-00095f043a13
+SCTX.RCTX: 7068f4c3-2612-44e6-9fc1-5266c37dbe92
+SCTX.PCTX: 7e1370ca-03f0-4565-99f5-4b022da51449
+RCTX.PCTX: 65603789-e110-455e-b142-e30705376481
+```
+
+```
+Controller: f98afab7-9557-43ec-9755-429734efff1d
+PCTX: fce145d7-8ddb-4290-9938-a30f0e7e7a69
+RCTX: 25171495-a704-48b4-930d-b199d7d6019c
+SCTX: 92d5c2f0-df3c-4fbb-95cf-00095f043a13
+SCTX.RCTX: 25171495-a704-48b4-930d-b199d7d6019c
+SCTX.PCTX: 7e1370ca-03f0-4565-99f5-4b022da51449
+RCTX.PCTX: fd678895-076e-4ad0-81a9-d913c613baa1
+```
--- src/main/java/jp/github/shibadog/samplespringscope/DemoApplication.java
+++ src/main/java/jp/github/shibadog/samplespringscope/DemoApplication.java
@@ -1,12 +1,116 @@
 package jp.github.shibadog.samplespringscope;
 
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.annotation.RequestScope;
+import lombok.extern.slf4j.Slf4j;
+
 
 @SpringBootApplication
+@Slf4j
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@RestController
+	public class DemoController {
+		private final RequestContext rctx;
+		private final SingletonContext sctx;
+		private final PrototypeContext pctx;
+		private final UUID uuid;
+
+		public DemoController(RequestContext rctx, SingletonContext sctx, PrototypeContext pctx) {
+			this.rctx = rctx;
+			this.sctx = sctx;
+			this.pctx = pctx;
+			this.uuid = UUID.randomUUID();
+		}
+
+		@RequestMapping(value="/test", method=RequestMethod.GET)
+		public String requestMethodName() throws InterruptedException {
+			TimeUnit.SECONDS.sleep(1L);
+			final String msg = String.format("Controller: %s\n"
+					+ "PCTX: %s\n"
+					+ "RCTX: %s\n"
+					+ "SCTX: %s\n"
+					+ "SCTX.RCTX: %s\n"
+					+ "SCTX.PCTX: %s\n"
+					+ "RCTX.PCTX: %s",
+					this.uuid.toString(),
+					pctx.getData(),
+					rctx.getData(),
+					sctx.getData(),
+					sctx.getRctx().getData(),
+					sctx.getPctx().getData(),
+					rctx.getPctx().getData());
+			log.info(msg);
+			return String.format(msg);
+		}
+		
+	}
+
+	@Component
+	@Scope("prototype")
+	public class PrototypeContext {
+		private final UUID uuid;
+		public PrototypeContext() {
+			uuid = UUID.randomUUID();
+		}
+
+		public String getData() {
+			return uuid.toString();
+		}
+	}
+
+	@Component
+	@RequestScope
+	public class RequestContext {
+		private final PrototypeContext pctx;
+		private final UUID uuid;
+		public RequestContext(PrototypeContext pctx) {
+			this.pctx = pctx;
+			uuid = UUID.randomUUID();
+		}
+
+		public String getData() {
+			return uuid.toString();
+		}
+
+		public PrototypeContext getPctx() {
+			return pctx;
+		}
+	}
+	
+	@Component
+	public class SingletonContext {
+		private final UUID uuid;
+		private final RequestContext rctx;
+		private final PrototypeContext pctx;
+		public SingletonContext(RequestContext rctx, PrototypeContext pctx) {
+			this.rctx = rctx;
+			this.pctx = pctx;
+			uuid = UUID.randomUUID();
+		}
+
+		public String getData() {
+			return uuid.toString();
+		}
+	
+		public RequestContext getRctx() {
+			return rctx;
+		}
+
+		public PrototypeContext getPctx() {
+			return pctx;
+		}
 	}
 }
